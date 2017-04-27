56ff22522caccc036ce3c183e760ef63ea17dbee
Zach Smith <zd@zdsmith.com>
2017-04-27 17:32:26 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+from setuptools import setup
+
+setup(
+    name='consul_srv',
+    version='0.1',
+    description='Consul SRV convenience module',
+    author='Zach Smith',
+    author_email='zach.smith@makespace.com',
+    license='Proprietary software - not for distribution',
+    packages=['consul_srv'])
