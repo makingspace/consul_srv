b092b86ad7ef20103e593b275eb9ecca2d6195aa
Zach Smith <zd@zdsmith.com>
2017-10-05 10:11:52 -0400

    Open source!

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 
 setup(
     name='consul_srv',
-    version='0.4.2',
+    version='0.4.3',
     description='Consul SRV convenience module',
     author='Zach Smith',
     author_email='zach.smith@makespace.com',
-    license='Proprietary software - not for distribution',
+    license='BSD3',
     packages=['consul_srv'],
     install_requires=["dnspython", "requests"])
