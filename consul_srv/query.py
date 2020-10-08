2be400873d048798448bbffbe8912ded6ad1022b
Joel Gilley <gilley.joel@gmail.com>
2020-10-08 16:44:54 -0400

    Fix

--- consul_srv/query.py
+++ consul_srv/query.py
@@ -45,12 +45,12 @@ class Resolver(Resolver):
         domain_name = "{}.{}".format(resource, self.consul_domain)
         try:
             answer = self.query(domain_name, "SRV", tcp=True)
-        except self.exception.Timeout:
+        except Resolver.Timeout:
             if(count<self.max_lookup):
                 count = count + 1
                 answer = self.get_service(resource, count)
             else:
-                raise self.exception.Timeout
+                raise Resolver.Timeout
 
         return answer
 
