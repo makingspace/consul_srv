f2e53c326a9e4bd3b3683b5af4cdb45e25d313f6
Joel Gilley <gilleyj@users.noreply.github.com>
2020-10-12 10:56:45 -0400

    Jgilley/excepttypo (#10)
    * Fix exception typo
    
    * Fix
    
    * Make the error more generic
    
    Co-authored-by: Joel Gilley <gilley.joel@gmail.com>

--- consul_srv/query.py
+++ consul_srv/query.py
@@ -45,12 +45,12 @@ class Resolver(Resolver):
         domain_name = "{}.{}".format(resource, self.consul_domain)
         try:
             answer = self.query(domain_name, "SRV", tcp=True)
-        except self.exception.Timeout:
+        except:
             if(count<self.max_lookup):
                 count = count + 1
                 answer = self.get_service(resource, count)
             else:
-                raise self.exception.Timeout
+                raise
 
         return answer
 
