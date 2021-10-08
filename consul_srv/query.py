ce330114963f4affd7997c1bca47fb1325568211
Santiago Moreno <smoreno@makespace.com>
2021-10-08 17:59:29 -0300

    Adding consul client as a failsafe option

--- consul_srv/query.py
+++ consul_srv/query.py
@@ -11,6 +11,7 @@ from typing import (
 
 import asyncio
 import aiodns
+import consul
 import pycares
 import time
 import random
@@ -27,13 +28,14 @@ class Resolver(aiodns.DNSResolver):
     def __init__(self, server_address: Optional[str] = None,
                  loop: Optional[asyncio.AbstractEventLoop] = None,
                  port = 53,
-                 timeout=2.0,
+                 client_port = 8500,
+                 timeout=1,
                  consul_domain='service.consul',
                  **kwargs: Any) -> None:
         self.loop = loop or asyncio.get_event_loop()
         assert self.loop is not None
         kwargs.pop('sock_state_cb', None)
-        self._channel = pycares.Channel(sock_state_cb=self._sock_state_cb, timeout=timeout, udp_port=port, **kwargs)
+        self._channel = pycares.Channel(sock_state_cb=self._sock_state_cb, tries=1, timeout=timeout, udp_port=port, **kwargs)
         if server_address:
             self.nameservers = [server_address]
         self._read_fds = set() # type: Set[int]
@@ -43,6 +45,7 @@ class Resolver(aiodns.DNSResolver):
         self.consul_address = '{}:{}'.format(server_address,port)
         self.consul_domain = consul_domain
         self.max_lookup = 5
+        self.client_port = client_port
 
     async def runQuery(self, name, query_type):
         return await self.query(name, query_type)
@@ -63,24 +66,42 @@ class Resolver(aiodns.DNSResolver):
 
         raise ValueError("No port information.")
 
-    def get_service(self, resource, count=0):
+    def get_service_from_consul(self, resource):
+        logging.debug('consul_srv: running Consul Client query')
+        client = consul.Consul(host=self.nameservers[0], port=self.client_port)
+        service_instances = client.catalog.service(resource)[1]
+        rand_instance = random.randint(0, len(service_instances)-1)
+        service_instance = service_instances[rand_instance]
+        return SRV(service_instance['ServiceAddress'], service_instance['ServicePort'])
+
+    def get_service_from_dns(self, resource):
+        logging.debug('consul_srv: running DNS query')
         domain_name = "{}.{}".format(resource, self.consul_domain)
+        coroSRV = self.runQuery(domain_name, 'SRV')
+        coroA = self.runQuery(domain_name, 'A')
+        answer = self.loop.run_until_complete(asyncio.gather(
+            coroSRV,
+            coroA
+        ))
+        return SRV(self._get_host(answer), self._get_port(answer))
+
+    def get_service(self, resource, count=0):
+        
         try:
-            logging.debug('consul_srv: requesting entries for {}'.format(domain_name))
-            coroSRV = self.runQuery(domain_name, 'SRV')
-            coroA = self.runQuery(domain_name, 'A')
-            answer = self.loop.run_until_complete(asyncio.gather(
-                coroSRV,
-                coroA
-            ))
+            logging.debug('consul_srv: requesting entries for {}'.format(resource))
+            answer = self.get_service_from_dns(resource)
         except:
-            if(count<self.max_lookup):
-                count = count + 1
-                logging.debug('consul_srv: exception, sleeping random 0-1 sec to try again, try {} of {}\n'.format(count, self.max_lookup))
-                time.sleep(random.random())
-                answer = self.get_service(resource, count)
-            else:
-                raise
+            try:
+                logging.debug('consul_srv: something odd happen while running DNS query, falling back to consul client')
+                answer = self.get_service_from_consul(resource)
+            except:
+                if(count<self.max_lookup):
+                    count = count + 1
+                    logging.debug('consul_srv: exception, sleeping random 0-1 sec to try again, try {} of {}\n'.format(count, self.max_lookup))
+                    time.sleep(random.random())
+                    answer = self.get_service(resource, count)
+                else:
+                    raise
         return answer
 
     def srv(self, resource):
@@ -89,10 +110,7 @@ class Resolver(aiodns.DNSResolver):
         named host/port tuple from the first element of the response.
         """
         # Get the host from the ADDITIONAL section
-        logging.debug('consul_srv: asked to lookup {} from {}\n'.format( resource, self.consul_address ))
-
+        logging.debug('consul_srv: asked to lookup {} from {}'.format( resource, self.consul_address ))
         answer = self.get_service(resource)
-        host = self._get_host(answer)
-        port = self._get_port(answer)
-        logging.info('consul_srv: recieved answer for {} as {}:{}\n'.format( resource, host, port ))
-        return SRV(host, port)
+        logging.info('consul_srv: recieved answer for {} as {}\n'.format( resource, answer ))
+        return answer
